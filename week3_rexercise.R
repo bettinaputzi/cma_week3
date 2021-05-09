4b231c3359f1529eaa3051fc13275e55606bab4f
Bettina Putzi <putzibet@students.zhaw.ch>
2021-05-09 17:38:33 +0200

    competed task 4

--- week3_rexercise.R
+++ week3_rexercise.R
@@ -17,13 +17,15 @@ caro <- caro %>%
     nPlus3  = sqrt((E-lead(E,3))^2+(N-lead(N,3))^2) # distance to pos +3 minutes
     )
 
-## Task  Specify and apply threshold d
+## Task 2: Specify and apply threshold d
 caro <- caro %>%
   rowwise() %>%
   mutate(
     stepMean = mean(c(nMinus3, nMinus2, nMinus1,nPlus1,nPlus2, nPlus3))
   ) %>%
   ungroup() 
+
+summary(caro)
 
 
 caro <- caro %>% 
@@ -39,4 +41,34 @@ p_caro<-ggplot(data=caro, aes(E,N))+
 p_caro
 
 
+## Task 4: Segment-based analysis
+rle_id <- function(vec){
+  x <- rle(vec)$lengths
+  as.factor(rep(seq_along(x), times=x))
+}
+
+caro <- caro%>%
+  mutate(segment_id = rle_id(static))
+caro
+
+
+caro_filter <- caro %>%
+  filter(!static)
+
+p2_caro<-ggplot(data=caro_filter, aes(E,N,color=segment_id))+
+  geom_point()+
+  geom_path()+
+  coord_equal()+
+  theme(legend.position = "bottom")
+p2_caro
+
+caro_filter_longsegements<-caro_filter[caro_filter$segment_id %in% names(which(table(caro_filter$segment_id) >6)), ]
+p3_caro<-ggplot(data=caro_filter_longsegements, aes(E,N,color=segment_id))+
+  geom_point()+
+  geom_path()+
+  coord_equal()+
+  theme(legend.position = "bottom")
+p3_caro
+
+# comment: I chose to filter out segments <=6 minutes. I have a bit different segments than the plots in the excercise-descripiton. This might come from different thresholds chosen...
 
